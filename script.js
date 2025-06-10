96c6c5aa4e0f3458894e4955d6fbed3b926f682c
Samuel <45376619+samuel-sfs@users.noreply.github.com>
2025-06-10 18:52:46 -0300

    Update JS com funções da página e validações do telefone

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
+
+let currentStep = 0;
+const formSteps = document.querySelectorAll('.form-step');
+const inputTelefone = document.getElementById('phone');
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+
+inputTelefone.addEventListener('input', function (e) {
+    let valor = e.target.value.replace(/\D/g, '');
+    let formatado = '';
+
+    if (valor.length > 0) {
+        formatado += '(' + valor.substring(0, 2);
+    }
+    if (valor.length > 2) {
+        formatado += ')' + valor.substring(2, 7);
+    }
+    if (valor.length > 7) {
+        formatado += '-' + valor.substring(7, 11);
+    }
+    e.target.value = formatado;
+});
+
+
+form.addEventListener('click', (e) => {
+    if (!e.target.matches('[data-action]')) {
+        return;
+    }
+
+    const actions = {
+        next() {
+            currentStep++;
+        },
+        prev() {
+            currentStep--;
+        }
+    }
+
+    const action = e.target.dataset.action;
+    actions[action]();
+    updateActiveStep();
+    updateProgressStep();
+
+});
+
+form.addEventListener('submit', (f) => {
+    f.preventDefault();
 })
 
+function updateActiveStep() {
+    formSteps.forEach(step => step.classList.remove('active'));
+    formSteps[currentStep].classList.add('active');
+}
 
+function updateProgressStep() { }
